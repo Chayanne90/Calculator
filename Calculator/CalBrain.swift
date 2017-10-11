1c78931061c23c8eb191171310f77a8c750dccd2
Chayanne90 <chayannepaulino@gmail.com>
2017-10-11 19:55:58 -0400

    bugs fix

--- Calculator/CalBrain.swift
+++ Calculator/CalBrain.swift
@@ -11,14 +11,13 @@ import Foundation
 struct CalculatorBrain {
     
     private var accumulator: Double?
+    var array:[String]  = [ ]
     
     private enum Operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double,Double) -> Double)
         case equals
-        case dot
-        
     }
     
     private var operations: Dictionary<String,Operation> = [
@@ -28,52 +27,72 @@ struct CalculatorBrain {
         "cos": Operation.unaryOperation(cos),
         "sin": Operation.unaryOperation(sin),
         "tan" : Operation.unaryOperation(tan),
-        "x²" : Operation.binaryOperation(pow),
+        "x²" : Operation.binaryOperation({pow($0,$1)}),
+        "x³" : Operation.unaryOperation({ pow($0, 3)}),
+        "x⁻¹" : Operation.unaryOperation({ 1 / $0 }),
+        "10ˣ" : Operation.unaryOperation({ pow(10, $0)}),
+        "ln" : Operation.unaryOperation(log),
         "±" : Operation.unaryOperation({-$0}),// closures
         "×" : Operation.binaryOperation({$0 * $1}),
         "÷" : Operation.binaryOperation({$0 / $1}),
         "+" : Operation.binaryOperation({$0 + $1}),
         "-" : Operation.binaryOperation({$0 - $1}),
         "=" : Operation.equals,
-        "." : Operation.dot,
-        
         ]
     
     mutating func performOperation( _ symbol: String){
-        
         if let operation =  operations[symbol]{
-            
             switch operation {
                 
             case .constant(let Value):
+                array.append(symbol)
                 accumulator = Value
             case .unaryOperation(let function):
                 if accumulator != nil{
+                    performPendingBinaryOperation()
                     accumulator = function(accumulator!)
                 }
             case .binaryOperation(let function):
+                
                 if accumulator != nil{
+                    performPendingBinaryOperation()
                     pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    array.append(symbol)
                     accumulator = nil
                 }
             case .equals:
                 performPendingBinaryOperation()
-            default:break
+                array.append(String("="))
             }
         }
     }
     
     // clear funciton "c"
     mutating func clear(){
-        accumulator = nil
+        accumulator = 0
         pendingBinaryOperation = nil
+        array.removeAll()
+        
     }
     
-
-        
+    var resultPending: Bool{
+        get{
+            if pendingBinaryOperation != nil{
+                return true
+            }
+            return false
+        }
+    }
     
+    var description: String{
+        mutating get{
+            if resultPending {
+                return array.joined(separator:"  ") + "..."
+            }
+            return array.joined(separator:"  ")
+        }
+    }
     private mutating func performPendingBinaryOperation(){
-        
         if pendingBinaryOperation != nil && accumulator != nil{
             accumulator = pendingBinaryOperation!.perform(with: accumulator!)
             pendingBinaryOperation = nil
@@ -83,24 +102,22 @@ struct CalculatorBrain {
     private var pendingBinaryOperation: PendingBinaryOperation?
     
     private struct PendingBinaryOperation{
-        
         let function: (Double, Double) -> Double
         let firstOperand: Double
         func perform(with secondOperand: Double) -> Double {
             return function(firstOperand, secondOperand)
-            
         }
-        
     }
+    
     mutating func setOperand(_ operand: Double){
         accumulator = operand
+        array.append(String(operand))
     }
+    
     var result: Double?{
         get{
-            
             return accumulator
         }
-        
     }
 }
 
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,77 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet var display: UILabel!
     @IBOutlet var display2: UILabel!
     var userTyping = false
- 
-  
-
+    
     @IBAction func TouchMe(_ sender: UIButton) {
-        
         let digit = sender.currentTitle!
+        
         if userTyping{
-            
             let currentDisplayByLabel = display!.text!
             display.text! = currentDisplayByLabel + digit
             display2.text! = currentDisplayByLabel + digit
             
-        }else{
-            
-            display2.text! = digit
+        }else {
             display.text! = digit
+            display2.text!  = digit
             userTyping = true
         }
         
-        
         if digit == "c"{
-
             display.text = "0"
             display2.text = " "
-            userTyping = false
             brain.clear()
+            userTyping = false
+        }
+        if digit == "."{         // " . " button better solution
+            display.text! = "."
+            display.text = "0\(digit)"
+            userTyping = true
         }
     }
     
-    // flaoting point button "."
-    @IBAction func dotAction(_ sender: Any) {
+    @IBAction func Undo(_ sender: UIButton) {
         
-        if (!display.text!.contains(".")){
-            display.text! = display.text! + String(".")
-            userTyping = true
+        if userTyping, var text = display.text{
+            text.remove(at: (text.index(before: (text.endIndex))))
+            display.text! = text
         }
     }
-
+    
     var displayValue: Double{
-        
         get{
-            
             return Double(display.text!)!
         }
         set{
-            
-            display.text! = String(format: "%6g",newValue)
+            display.text!  = String(format: "%6g",newValue)
+            display2.text! = String(format: "%6g")
         }
     }
     
-
     private var brain = CalculatorBrain()
+    
     @IBAction func Perform(_ sender: UIButton){
-        
         if userTyping{
-            
             brain.setOperand(displayValue)
             userTyping = false
         }
-        if let mathSymbol = sender.currentTitle{
-            
+        if  let mathSymbol = sender.currentTitle{
             brain.performOperation(mathSymbol)
+            display2.text = brain.description
         }
+        
         if let result = brain.result{
-            
             displayValue = result
         }
-    
+        display2.text! = brain.description
     }
 }
 
